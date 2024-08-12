3c7592e709756a8a33028c3a1d239243dbe87e59
ShantanuSR <113551085+ShantanuSR@users.noreply.github.com>
2024-08-13 00:35:41 +0530

    Update MovieStore.cs

--- MovieApp/MovieStore.cs
+++ MovieApp/MovieStore.cs
@@ -28,21 +28,21 @@ namespace MoviesApp
 
             char choice = 'y';
 
-           do
-            {
-                Console.WriteLine("Total available movies : " + MovieManager.movies.Count);
-                Console.WriteLine("1. Add Movie");
-                Console.WriteLine("2. Display Movie");
-                Console.WriteLine("3. Delete Movie");
-                Console.WriteLine("4. Exit ");
-                Console.WriteLine("Enter you choice : ");
-                int inputNum = int.Parse(Console.ReadLine());
-                movieManager.MainMenu(inputNum);
-                Console.WriteLine();
-                Console.WriteLine("Do you want to continue Y/N ?");
-                choice = char.Parse(Console.ReadLine());
-            } while (choice == 'Y' || choice == 'y') ;
-
+do
+ {
+     Console.WriteLine("Total available movies : " + MovieManager.movies.Count);
+     Console.WriteLine("1. Add Movie");
+     Console.WriteLine("2. Display Movies");
+     Console.WriteLine("3. Delete Movie");
+     Console.WriteLine("4. Delete all movies");
+     Console.WriteLine("5. Exit ");
+     Console.WriteLine("Enter you choice : ");
+     int inputNum = int.Parse(Console.ReadLine());
+     movieManager.MainMenu(inputNum);
+     Console.WriteLine();
+     Console.WriteLine("Do you want to continue Y/N ?");
+     choice = char.Parse(Console.ReadLine());
+ } while (choice == 'Y' || choice == 'y') ;
 
         }
 
